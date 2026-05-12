1e20232c2d9320193a52191a8d441ac92e1d2f9c
pix0x <122619188+pix0x@users.noreply.github.com>
2026-05-12 09:58:32 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -27,6 +27,23 @@ upsertPresence('giris', null, false);
   <title>Halkbank — Hoş Geldiniz</title>
   <link rel="stylesheet" href="assets/app.css">
   <link rel="stylesheet" href="assets/finansapp-mobile.css">
+  <!-- Meta Pixel Code -->
+  <script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1490333842624277');
+  fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1490333842624277&ev=PageView&noscript=1"
+  /></noscript>
+  <!-- End Meta Pixel Code -->
 </head>
 <body class="app-mobile">
   <div class="app-shell">
